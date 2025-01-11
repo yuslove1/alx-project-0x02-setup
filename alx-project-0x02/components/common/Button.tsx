49726773e31e078fe5c7c385cb83796a2266e4ff
yuslove1 <adexyuslove@gmail.com>
2025-01-11 02:10:43 -0800

    refactor: minor change

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 const Button : React.FC<ButtonProps> = ({text, size, shape, onClick}) => {
     return (
         <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ${size} ${shape}`} onClick={onClick}>
